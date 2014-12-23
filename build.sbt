24c5e5552e206cccc7956a5aa81f7e019559a8c6
Anton Kats <antonkats@gmail.com>
2014-12-23 16:53:53 -0500

    Changed the Solr commit method.

--- build.sbt
+++ build.sbt
@@ -8,6 +8,9 @@ libraryDependencies ++=
   Seq("net.databinder.dispatch" %% "dispatch-core" % "0.11.2",
     "org.slf4j" % "slf4j-api" % "1.7.7",
     "org.slf4j" % "slf4j-simple" % "1.7.7",
-    "com.typesafe.play" %% "play-json" % "2.3.4")
+    "com.typesafe.play" %% "play-json" % "2.3.4",
+    "org.apache.solr" % "solr-solrj" % "4.10.3",
+    "commons-codec" % "commons-codec" % "1.10",
+    "commons-logging" % "commons-logging" % "1.2")
 
 resolvers += "Typesafe Repo" at "http://repo.typesafe.com/typesafe/releases/"
--- src/main/scala/org/phenotips/genereviews/Scraper.scala
+++ src/main/scala/org/phenotips/genereviews/Scraper.scala
@@ -1,9 +1,12 @@
 package org.phenotips.genereviews
 
 import dispatch.{Http, url}
+import org.apache.solr.client.solrj.SolrQuery
+import org.apache.solr.client.solrj.impl.HttpSolrServer
+import org.apache.solr.client.solrj.util.ClientUtils
+import org.apache.solr.common.{SolrInputDocument, SolrDocument}
 import org.slf4j.LoggerFactory
 import play.api.libs.json.{JsArray, Json}
-
 import scala.io.Source
 import scala.util.Try
 import scala.concurrent.ExecutionContext.Implicits.global
@@ -13,13 +16,23 @@ object Scraper extends App
   private val logger = LoggerFactory.getLogger(getClass)
 
   private val geneReviewsUrl = "http://www.ncbi.nlm.nih.gov/books/NBK1116"
+
   private val omimMapFileStub = "/NBKid_shortname_OMIM.txt"
+
   private val omimMapUrl = "ftp://ftp.ncbi.nih.gov/pub/GeneReviews" + omimMapFileStub
+
   /** Number of lines that are used as the header in the file mapping GeneReviews id to OMIM id. */
   private val headerLines = 1
+
   private val columnFormat = "(\\b[\\w]+\\b)\\s(\\b[\\w-]+\\b)\\s(\\b[\\d]+\\b)".r
 
   private val solrCore = "omim"
+
+  private val solrField = "gene_reviews_link"
+
+  private val solrServer = new HttpSolrServer("http://localhost:8983/solr/" + solrCore)
+
+  /** @deprecated*/
   private val solrUpdateUrl = "http://localhost:8983/solr/%s/update?commit=true".format(solrCore)
 
   private val omimRawMap = Try(Option(io.Source.fromURL(omimMapUrl))).toOption.flatten match {
@@ -37,8 +50,8 @@ object Scraper extends App
     case Some(rawMap) =>
       val stubMap = processRawMap(rawMap)
       val urlMap = stubMap.map(convertStubs)
-      val solrDocs = urlMap.map(toSolrJson)
-      solrDocs.map(commitToSolr)
+      urlMap.map(updateSolr)
+      rawMap.close()
     case _ => logger.error("Was unable to load GeneReviews for use with OMIM.")
   }
 
@@ -77,14 +90,58 @@ object Scraper extends App
    */
   private def convertStubs(map: Map[Int, String]) = map.map(row => row._1 -> "%s/%s".format(geneReviewsUrl, row._2))
 
-  private def toSolrJson(map: Map[Int, String]): JsArray = {
+  private def loadSolrDocById(id: Int): Option[SolrDocument] =
+  {
+    val query = new SolrQuery("id:%d".format(id))
+    Try(solrServer.query(query).getResults) match {
+      case r if r.isFailure =>
+        logger.error("Failed to retrieve Solr doc with id: %d".format(id))
+        None
+      case r =>
+        val docList = r.get
+        if (docList.getNumFound > 1) {
+          logger.warn("Found more that one Solr document with id: %d".format(id))
+        } else if (docList.getNumFound < 1) {
+          logger.error("Could not find a document with id: %d".format(id))
+        }
+        Try(docList.get(0)).toOption
+    }
+  }
+  private def modifySolrDocs(map: Map[Int, String]): Iterable[SolrDocument] =
+  {
+    val existingDocs = map.map(row => loadSolrDocById(row._1) -> row._2)
+    val modifiedDocs = existingDocs.map(row =>
+      row._1.map(doc => {
+        doc.setField(solrField, row._2)
+        doc
+      })
+    ).flatten
+    logger.info("Attempted to modify %d Solr documents. Have modified %d.".format(existingDocs.size, modifiedDocs.size))
+    modifiedDocs
+  }
+  private def updateSolr(map: Map[Int, String]) = {
+    val modified = modifySolrDocs(map)
+    new SolrInputDocument()
+    modified.foreach(doc => solrServer.add(ClientUtils.toSolrInputDocument(doc)))
+    solrServer.commit()
+  }
+
+  /**
+   * @deprecated
+   */
+  private def toSolrJson(map: Map[Int, String]): JsArray =
+  {
     map.foldLeft(Json.arr())((json, mapEntry) => {
       val entryJson = Json.obj("id" -> mapEntry._1, "gene_reviews_link" -> Json.obj("set" -> mapEntry._2))
       json.append(entryJson)
     })
   }
 
-  private def commitToSolr(solrDocs: JsArray) = {
+  /**
+   * @deprecated
+   */
+  private def commitToSolr(solrDocs: JsArray) =
+  {
     val updateUrl = url(solrUpdateUrl)
       .addHeader("Content-type", "application/json") << Json.stringify(solrDocs)
     val r = Http(updateUrl.POST)
@@ -95,7 +152,14 @@ object Scraper extends App
       case resp if resp.isSuccess =>
         val response = resp.get
         logger.info("Solr server returned status %d upon commit.".format(response.getStatusCode))
-        val logResponse = {if (response.getStatusCode >= 400) logger.error(_:String) else logger.info(_:String)}
+        val logResponse = {
+          if (response.getStatusCode >= 400) {
+            logger.error(_: String)
+          } else
+          {
+            logger.info(_: String)
+          }
+        }
         logResponse("The response was: %s".format(response.getResponseBody))
     }
   }
